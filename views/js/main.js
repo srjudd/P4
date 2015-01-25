9e8027a527e16670884335aa9ef3a94fd82e0dcb
Renee Judd <rjudd@nl.edu>
2015-01-25 09:24:51 -0600

    Making number of moving pizzas global, saving iteration of array length determination. Taking constant part of phase calculation out of iteration.

--- views/js/main.js
+++ views/js/main.js
@@ -18,6 +18,7 @@ cameron *at* udacity *dot* com
 
 // As you may have realized, this website randomly generates pizzas.
 // Here are arrays of all possible pizza ingredients.
+var pnum = 100;
 var pizzaIngredients = {};
 pizzaIngredients.meats = [
   "Pepperoni",
@@ -450,11 +451,12 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
-    }
+//    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
+//      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
+//     var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
+//     document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+//   }
+console.log("changing size")
   }
 
   changePizzaSizes(size);
@@ -501,10 +503,10 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
-
   var items = document.querySelectorAll('.mover');
-  for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+  var scrolly = (document.body.scrollTop / 1250);
+  for (var i = 0; i < pnum; i++) {
+    var phase = Math.sin(scrolly + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
@@ -525,7 +527,8 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 200; i++) {
+
+  for (var i = 0; i < pnum; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
