a3dc052043596d61e916b795bf0a9ccc7a184553
Renee Judd <rjudd@nl.edu>
2015-01-28 23:43:22 -0600

    Removed calculation and look-up from for-loop in resizing pizzas

--- views/js/main.js
+++ views/js/main.js
@@ -450,15 +450,28 @@ var resizePizzas = function(size) {
   }
 
   // Iterates through pizza elements on the page and changes their widths
-  function changePizzaSizes(size) {
+ /* function changePizzaSizes(size) {
+    var dx = determineDx(document.querySelector(".randomPizzaContainer"), size);
+    var newwidth = (document.querySelectorAll(".randomPizzaContainer").offsetWidth + dx) + 'px';
     for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-     var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
+     
+     
     document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
    }
 console.log("changing size")
-  }
+  }*/
 
+function changePizzaSizes(size) {
+  var rpcLength = document.querySelectorAll(".randomPizzaContainer").length;
+  var dx = determineDx(document.querySelector(".randomPizzaContainer"), size);
+  var newwidth = (document.querySelector(".randomPizzaContainer").offsetWidth + dx) + 'px';
+   for (var i = 0; i < rpcLength; i++) {
+     //var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
+    //var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
+   document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+   }
+console.log("changing size")
+}
   changePizzaSizes(size);
 
   // User Timing API is awesome
