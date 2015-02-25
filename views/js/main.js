9a412aa87e1b4c37a362e816b00d8173b0d9354b
Renee Judd <rjudd@nl.edu>
2015-02-25 01:51:57 -0600

    compressed pizzeria.jpg, changed pnum to 49

--- views/js/main.js
+++ views/js/main.js
@@ -18,7 +18,7 @@ cameron *at* udacity *dot* com
 
 // As you may have realized, this website randomly generates pizzas.
 // Here are arrays of all possible pizza ingredients.
-var pnum = 50;
+var pnum = 49;
 var pizzaIngredients = {};
 pizzaIngredients.meats = [
   "Pepperoni",
